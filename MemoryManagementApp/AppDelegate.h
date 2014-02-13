59f8ed268140df65a4a59763cea754bba4cb8dd5
Bennett Lin <bennettslin@gmail.com>
2014-02-13 13:55:27 -0800

    Initial Commit

---
+++ MemoryManagementApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MemoryManagementApp
+//
+//  Created by Bennett Lin on 2/13/14.
+//  Copyright (c) 2014 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MemoryManagementApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MemoryManagementApp
+//
+//  Created by Bennett Lin on 2/13/14.
+//  Copyright (c) 2014 Bennett Lin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
